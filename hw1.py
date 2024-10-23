9871cba6b23ca55e636b11f173366648f8e54139
iacopocagnoni <icagnoni@calpoly.edu>
2024-10-22 22:13:31 -0700

    Commit 1

--- hw1.py
+++ hw1.py
@@ -3,26 +3,91 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def vowel_count(input_str) -> int:                          #the vowel_count function return the number of vowels in an inputted string.
+                                                            #Input: (type: str). the function will count the vowels. Output: integer, the number of vowels in the input string.
+   vowels = "aeiouAEIOU"                                    #The parameter input_str is of type str. The return type is int.
+   return sum (1 for char in input_str if char in vowels)
 
 # Part 2
-
+def short_lists(input_list: list[list[int]]) -> list[list[int]]:   #The function short_lists returns a new list consisting of those elements of the input list (in the same order) that are of length 2.
+                                                                   #Input: input_lists, a list containing nested lists of integers. Output: A list containing only the sub lists from input_list that have exactly 2 elements
+    return [lst for lst in input_list if len(lst) == 2]            #parameters: input_list, a list containing sub lists of integers. Return type: A list of sub lists, where each sublist has two integers.
 
 # Part 3
-
+def ascending_pairs(pairs_list: list[list[int]]) -> list[list[int]]: #The function ascending_pairs purpose is to return a new list in which each sublist of length 2 is sorted from lowest to highest.
+    result = []                                                      #Input: pairs_list, a list of sub lists that have integers. Output: a new list where all sub lists are ordered lowest to highest.
+    for lst in pairs_list:                                           #To show that the input parameter pairs_list is a list of sub lists with integers, it is annotated as list[list[int]].
+        if len(lst) == 2:                                            #The return type is list[list[int]], indicating that the function returns a list of sub lists of integers.
+            result.append(sorted(lst))
+        else:
+            result.append(lst)
+    return result
 
 # Part 4
+class Price:
+    def __init__(self, dollars: int, cents: int):
+        self.dollars = dollars
+        self.cents = cents
 
+def add_prices(price1, price2):                         #the function add_prices adds two prices, and when the cents go over 99, then it converts it to a dollar. The function returns a new Price object representing the sum of the two input prices.
+    total_cents = price1.cents + price2.cents           #input: price1 and price2, object with type Price with cents and dollars. Output: A new Price object gotten from total of dollars and cents from price 1 and price2
+    total_dollars = price1.dollars + price2.dollars + total_cents // 100 # type annotations: function adds cents and dollars from price1 and price2. Converts over 99 cents into dollars and returns a Price object.
+    total_cents = total_cents % 100
+    return Price(total_dollars, total_cents)
 
 # Part 5
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+class Rectangle:
+    def __init__(self, top_left: Point, bottom_right: Point):
+        self.top_left = top_left
+        self.bottom_right = bottom_right
+
+def rectangle_area(rectangle: Rectangle) -> int:         #the function rectangle_area returns the area of a rectangle represent by top_left corner and bottom_right.
+    width = rectangle.bottom_right.x - rectangle.top_left.x   #input: rectangle: object Rectangle containing the objects top_left and bottom_right. They have x and y coordinates.
+                                                              #output: area of the rectangle as an int, a product of width and height.
+    height = rectangle.top_left.y - rectangle.bottom_right.y  #type annotations: the rectangle's area is an int to match the return type
+
+    return width * height
 
 
 # Part 6
+class Book:
+    def __init__(self, authors: list[str], title: str):
+        self.authors = authors
+        self.title = title
 
-
+def books_by_author(author_name: str, books_list: list[Book]) -> list[Book]:    #the books_by_author function returns a list of books written by a certain author from a list of Book
+    return [book for book in books_list if author_name in book.authors]         #input: author_name: a string with a name of the author and book_list: A list of Book objects with title and list of authors
+                                                                                #output: a list of Book and author_name
+                                                                                #type annotations: author_name is a str, and book_list is a list of Book. Return type: function returns a list of Book
 # Part 7
+class Circle:                                                           #the circle_bound returns a circle from the rectangle provided. The radius is the distance from the center to the corner of the rectangle.
+    def __init__(self, center: Point, radius: float):                   #input: rectangle of type Rectangle which will be used to find the bounding circle.
+        self.center = center                                            #output: Circle object. the center of circle is the center of the rectangle.
+        self.radius = radius
 
+def circle_bound(rectangle: Rectangle) -> Circle:   #type annotations:
+    center_of_rectangle = rectangle.center()        #uses the center point of the rectangle
+    radius_of_circle = rectangle.distance_to(rectangle.top_right) #uses center to top-right corner
+    return Circle(center_of_rectangle, radius_of_circle)
 
 # Part 8
+class Employee:
+    def __init__(self, name: str, pay_rate: int):
+        self.name = name
+        self.pay_rate = pay_rate
+
+def below_pay_average(employees: list[Employee]) -> list[str]:  #the function below_pay_average is used to identify employees that get paid less than average among the employees on the list.
+    if not employees:                                           #input: list of Employee objects with the attributes name, name of the employee and pay_rate the pay of the employee as an integer.
+        return []                                               #output: list of strings with name of employees who have a below pay rate than the others in the list. If empty, then it returns an empty list.
+    total_pay = sum(employee.pay_rate for employee in employees) #type annotations: list[Employee]: The function expects a list of Employee objects. list -> str: function return a list of strings with employees who get paid less than average.
+    average_pay = total_pay / len(employees)
+
+    below_average = [employee.name for employee in employees if employee.pay_rate < average_pay]
+
+    return below_average
 
 
--- hw1_test.py
+++ hw1_test.py
@@ -1,37 +1,108 @@
 import data
 import hw1
 import unittest
-
+from math import sqrt
 
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
     # Part 1
+    def test_empty_string(self):
+        self.assertEqual(hw1.vowel_count(""), 0)
 
+    def test_uppercase_vowels(self):
+        self.assertEqual(hw1.vowel_count("AEIOU"), 5)
 
     # Part 2
+    def test_length_2(self):
+        self.assertEqual(hw1.short_lists([[1,2], [3, 4]]), [[1, 2], [3, 4]])
 
+    def test_empty_list(self):
+        input_data = []
+        expected_output = []
+        self.assertEqual(hw1.short_lists(input_data), expected_output)
 
     # Part 3
+    def test_mixed_length(self):
+        input_data = [[4, 1], [3], [2, 5], [6, 7, 8], [10, 5]]
+        expected_output = [[1, 4], [3], [2, 5], [6, 7, 8], [5, 10]]
+        self.assertEqual(hw1.ascending_pairs(input_data), expected_output)
 
+    def test_no_length_2(self):
+        input_data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        expected_output = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        self.assertEqual(hw1.ascending_pairs(input_data), expected_output)
 
     # Part 4
 
+    def test_add_prices_1(self):
+        price1 = hw1.Price(9, 45)
+        price2 = hw1.Price(7, 27)
+        result = hw1.add_prices(price1, price2)
+        self.assertEqual(result.dollars, 16)
+        self.assertEqual(result.cents, 72)
+
+    def test_add_prices_2(self):
+        price1 = hw1.Price(11, 90)
+        price2 = hw1.Price(6, 80)
+        result = hw1.add_prices(price1, price2)
+        self.assertEqual(result.dollars, 18)
+        self.assertEqual(result.cents, 70)
+
 
     # Part 5
+    def test_Rectangle_1(self):
+        top_left = hw1.Point(0,0)
+        bottom_right = hw1.Point(1,0)
+        rectangle_with_side_equal_zero = hw1.Rectangle(top_left, bottom_right)
+        result = hw1.rectangle_area(rectangle_with_side_equal_zero)
+        self.assertEqual(result, 0)
+
+    def test_Rectangle_2(self):
+        top_left = hw1.Point(3, 6)
+        bottom_right = hw1.Point(6, 3)
+        rectangle1 = hw1.Rectangle(top_left, bottom_right)
+        result = hw1.rectangle_area(rectangle1)
+        self.assertEqual(result, 9)
 
 
     # Part 6
+    def test_books_by_author_1(self):
+        authors = ["A", "B", "C"]
+        title = "D"
+        book1 = hw1.Book(authors, title)
+        book2 = hw1.Book(["A", "B"], "Grapes")
+        result = hw1.books_by_author("C", [book1, book2])
+        self.assertEqual(result, [book1])
 
+    def test_books_by_author_2(self):
+        book1 = hw1.Book(["E", "F", "G"], "Guitar")
+        book2 = hw1.Book(["E", "F", "G"], "Piano")
+        result = hw1.books_by_author("H", [book1, book2])
+        self.assertEqual(result, [])
 
     # Part 7
+    def test_circle_1(self):
+        circle = data.Circle(data.Point(15, 30), 9.5)
+        self.assertEqual(circle.center, data.Point(15, 30))
+        self.assertEqual(circle.radius, 9.5)
+
+    def test_circle_2(self):
+        circle1 = data.Circle(data.Point(10, 15), 5.3)
+        circle2 = data.Circle(data.Point(8, 12), 5.3)
+        self.assertNotEqual(circle1, circle2)
 
 
     # Part 8
+    def test_employee_1(self):
+        employee = data.Employee('Jake', 7)
+        self.assertEqual(employee.name, 'Jake')
+        self.assertEqual(employee.pay_rate, 7)
 
-
-
-
+    def test_employee_2(self):
+        employees = []
+        result = hw1.below_pay_average(employees)
+        self.assertEqual(result, [])
 
 if __name__ == '__main__':
     unittest.main()
